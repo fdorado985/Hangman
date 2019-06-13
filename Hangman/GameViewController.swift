7dc165e51b9b06339ec3fdba99918518c933e694
Juan Francisco Dorado Torres <fdorado985@gmail.com>
2019-06-12 20:46:51 -0500

    Add the initial state of the game

--- Hangman/GameViewController.swift
+++ Hangman/GameViewController.swift
@@ -10,10 +10,48 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+  // MARK: - Outlets
+
+  @IBOutlet var scoreLabel: UILabel!
+  @IBOutlet var wordLabel: UILabel!
+
+  // MARK: - Properties
+
+  private var words = [String]()
+  private var wordToGuess = ""
+  private var maskedWord = ""
+  private var wordIndex = 0
+  private var score = 0
+
   // MARK: - View cycle
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    startGame()
+  }
+
+  // MARK: - Methods
+
+  private func getWords() {
+    if let filePath = Bundle.main.url(forResource: "words", withExtension: "txt"),
+      let wordsOfPath = try? String(contentsOf: filePath).trimmingCharacters(in: .whitespacesAndNewlines) {
+      words = wordsOfPath.components(separatedBy: "\n")
+      print(words)
+    } else {
+      words.append("Apple")
+    }
+  }
+
+  private func startGame() {
+    getWords()
+    scoreLabel.text = String(score)
+    words.shuffle()
+    wordToGuess = words[wordIndex]
+    for _ in wordToGuess {
+      maskedWord.append(Character("?"))
+    }
+
+    wordLabel.text = maskedWord
   }
 }
 
